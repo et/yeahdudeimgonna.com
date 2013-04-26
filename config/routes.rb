dd8eb15f2d7b4fa3112d0c286c5bc172da71de83
Eric Thomas <eric.l.m.thomas@gmail.com>
2013-04-25 17:49:14 -0700

    update from kracker's house

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ YeahdudeimgonnaCom::Application.routes.draw do
   get 'about'      => 'high_voltage/pages#show', id: 'about'
   get 'home'       => 'high_voltage/pages#show', id: 'home'
   get 'statistics' => 'high_voltage/pages#show', id: 'statistics'
+  get 'See the seahorses at the Monterey Bay Aquarium and Backpack to the Big Sur hot springs and Run the beaches of Santa Barbara and Get a photo with Mickey at Disneyland'.parameterize => 'high_voltage/pages#show', id: 'update1'
 
   get ':slug' => 'published_itineraries#show', as: :published_itinerary
   root to: 'high_voltage/pages#show', id: 'home'
